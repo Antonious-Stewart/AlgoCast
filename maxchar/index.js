b76c30e94c68dc56ee039c92a263cb052b47e07b
antonious-stewart <antonious.stewart@gmail.com>
2019-10-21 09:29:50 -0400

    maxChar complete

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+	const charMap = {};
+	let count = 0;
+	let maxChar = '';
+	for (let char of str.replace(/\s+/gm, '')) {
+		// assign char to charMap
+		// when iterating char for first time asign 1 to value
+		// when iteration occurs again increment
+		charMap[char] = charMap[char] + 1 || 1;
+	}
+	for (let char in charMap) {
+		//if count is less than charMap[char] count will equal count
+		//maxChar will equal char that occurs the most
+		if (count < charMap[char]) {
+			count = charMap[char];
+			maxChar = char;
+		}
+	}
+	//return most reoccurring char
+	return maxChar;
+}
+maxChar('Hello There');
 module.exports = maxChar;
